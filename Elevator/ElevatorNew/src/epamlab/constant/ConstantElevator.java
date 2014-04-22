a9c0988077d6d92b0d8d4b5f1baf2db74b64c98a
Siarhei Dauhachou <deliaserik@gmail.com>
2014-04-23 00:40:13 +0300

    New

--- Elevator/ElevatorNew/src/epamlab/constant/ConstantElevator.java
+++
@@ -1,7 +0,0 @@
-package epamlab.constant;
-public class ConstantElevator {
-	public static int storiesNumber;
-	public static int  elevatorCapacity;
-	public static int   passengersNumber;
-    public static int   animationBoost;
-}
